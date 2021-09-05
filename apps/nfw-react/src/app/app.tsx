f36bd8781a4778939947a4aac986d50ace8b2498
Michal Dzik <michal.dzik@vizlib.com>
2021-09-05 20:50:15 +0200

    chore(api): cors

--- apps/nfw-react/src/app/app.tsx
+++ apps/nfw-react/src/app/app.tsx
@@ -1,15 +1,8 @@
 import React from 'react';
 
 export const App = () => {
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./app.styled-components file.
-   */
   return (
-    <div>
-      Playground
-    </div>
+    <p>Starting point</p>
   );
 };
 
